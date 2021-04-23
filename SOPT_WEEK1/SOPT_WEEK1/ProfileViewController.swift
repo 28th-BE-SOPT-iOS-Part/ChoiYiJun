3d198b9c0fa626abf11ef309406335772066fb94
Yi Joon Choi <choi990406@gmail.com>
2021-04-23 23:04:41 +0900

    0423 2주차 도전과제

--- SOPT_WEEK1/SOPT_WEEK1/ProfileViewController.swift
+++ SOPT_WEEK1/SOPT_WEEK1/ProfileViewController.swift
@@ -23,8 +23,36 @@ class ProfileViewController: UIViewController {
 
         view.backgroundColor = UIColor.bluishGrey
         line.backgroundColor = UIColor.coolGrey
-        // Do any additional setup after loading the view.
+        
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerHandler(_:)))
+           view.addGestureRecognizer(gestureRecognizer)
     }
     
+    @IBAction func panGestureRecognizerHandler(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: view?.window)
+        var initialTouchPoint = CGPoint.zero
+
+        switch sender.state {
+        case .began:
+            initialTouchPoint = touchPoint
+        case .changed:
+            if touchPoint.y > initialTouchPoint.y {
+                view.frame.origin.y = touchPoint.y - initialTouchPoint.y
+            }
+        case .ended, .cancelled:
+            if touchPoint.y - initialTouchPoint.y > 200 {
+                dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.view.frame = CGRect(x: 0,
+                                             y: 0,
+                                             width: self.view.frame.size.width,
+                                             height: self.view.frame.size.height)
+                })
+            }
+        case .failed, .possible:
+            break
+        }
+    }
 
 }
